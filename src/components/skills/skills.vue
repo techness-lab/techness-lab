de0e332251dcb43992a02d28e20734ff34cc93de
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-10-02 14:17:21 +0300

    Fix progress section registry issue

--- src/components/skills/skills.vue
+++ src/components/skills/skills.vue
@@ -1,10 +1,10 @@
-<template src="./progress.html">
+<template src="skills.html">
     
 </template>
 
 <script>
     export default {
-        name: "progress"
+        name: "skills"
     }
 </script>
 

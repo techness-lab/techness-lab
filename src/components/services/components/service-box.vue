b63df92d36527c13b9c6b77c30007a0d6236c012
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-10-03 20:04:50 +0300

    Create a separate component for service box

--- src/components/services/components/service-box.vue
+++ src/components/services/components/service-box.vue
@@ -1,13 +1,17 @@
-<template>
-    $END$
+<template src="./service-box.html">
 </template>
 
 <script>
     export default {
-        name: 'service-box'
+        name: 'service-box',
+        props: ['imageUrl', 'heading', 'description'],
+        data() {
+            return {
+                publicPath: process.env.BASE_URL
+            };
+        }
     };
 </script>
 
 <style scoped>
-
 </style>
--- src/components/services/services.vue
+++ src/components/services/services.vue
@@ -1,16 +1,20 @@
 <template src="./services.html">
-    
 </template>
 
 <script>
+    import ServiceBox from './components/service-box';
+
     export default {
-        name: "services",
+        name: 'services',
+        components: {
+            ServiceBox
+        },
         data() {
             return {
                 publicPath: process.env.BASE_URL
             };
         }
-    }
+    };
 </script>
 
 <style scoped>

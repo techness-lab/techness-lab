d3dec423d00eb7c15954d4f8feb864a227cb7df5
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-10-03 22:51:50 +0300

    Create separate component for education item

---
+++ src/components/education-section/components/education-item.vue
@@ -0,0 +1,21 @@
+<template src="./education-item.html">
+</template>
+
+<script>
+    export default {
+        name: 'education-item',
+        props: {
+            mainYear: Number,
+            rank: Number,
+            label: String,
+            labelBackground: String,
+            diploma: String,
+            curriculum: String,
+            details: String
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
--- src/components/education-section/education-section.vue
+++ src/components/education-section/education-section.vue
@@ -1,11 +1,54 @@
 <template src="./education-section.html">
-    
+
 </template>
 
 <script>
+    import EducationItem from './components/education-item';
+
     export default {
-        name: "EducationSection"
-    }
+        name: 'EducationSection',
+        components: {
+            EducationItem
+        },
+        data() {
+            return {
+                educations: [
+                    {
+                        mainYear: 2019,
+                        label: 'United States',
+                        labelBackground: 'bg-pink',
+                        diploma: 'Diploma',
+                        curriculum: 'In UI/UX Design',
+                        details: 'Sed ut perspiciatis unde omnis iste natus error a sit voluptatem accusantium doloremque are they laudantium,totam rem aperiam'
+                    },
+                    {
+                        mainYear: 2018,
+                        label: 'New York',
+                        labelBackground: 'bg-yellow',
+                        diploma: 'Diploma',
+                        curriculum: 'In Web Design',
+                        details: 'Sed ut perspiciatis unde omnis iste natus error a sit voluptatem accusantium doloremque are they laudantium,totam rem aperiam'
+                    },
+                    {
+                        mainYear: 2017,
+                        label: 'Canada',
+                        labelBackground: 'bg-orange',
+                        diploma: 'Diploma',
+                        curriculum: 'In Angular',
+                        details: 'Sed ut perspiciatis unde omnis iste natus error a sit voluptatem accusantium doloremque are they laudantium,totam rem aperiam'
+                    },
+                    {
+                        mainYear: 2016,
+                        label: 'United States',
+                        labelBackground: 'bg-cyan',
+                        diploma: 'Diploma',
+                        curriculum: 'In Angular',
+                        details: 'Sed ut perspiciatis unde omnis iste natus error a sit voluptatem accusantium doloremque are they laudantium,totam rem aperiam'
+                    },
+                ]
+            };
+        }
+    };
 </script>
 
 <style scoped>

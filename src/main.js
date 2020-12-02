08d87a45afd7a675670dc5bbb1d38b4e313cb147
Randriantsarafara Ny Hasinavalona <hrandriantsarafara@bocasay.com>
2020-12-02 23:07:56 +0300

    Use vue ellipse progress

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import VueTypedJs from 'vue-typed-js';
+import VueEllipseProgress from 'vue-ellipse-progress';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(VueTypedJs);
+Vue.use(VueEllipseProgress);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+}).$mount('#app');

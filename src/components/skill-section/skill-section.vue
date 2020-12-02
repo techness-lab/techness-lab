6f938ede2413f3fb0d574af16b6c744dfda9fa80
Randriantsarafara Ny Hasinavalona <hrandriantsarafara@bocasay.com>
2020-12-02 23:44:59 +0300

    Dynamize skills generation

--- src/components/skill-section/skill-section.vue
+++ src/components/skill-section/skill-section.vue
@@ -4,7 +4,29 @@
 
 <script>
     export default {
-        name: "SkillSection"
+        name: "SkillSection",
+        data() {
+            return ({
+                skills: [
+                    {
+                        progress: 92,
+                        value: `Projects`
+                    },
+                    {
+                        progress: 100,
+                        value: `Team Work`
+                    },
+                    {
+                        progress: 95,
+                        value: `Happy Clients`
+                    },
+                    {
+                        progress: 80,
+                        value: `Creativity`
+                    }
+                ]
+            });
+        }
     }
 </script>
 

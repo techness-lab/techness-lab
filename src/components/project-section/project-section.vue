4cee662b513db415686b04cb846f9e7726192bc2
Randriantsarafara Ny Hasinavalona <hrandriantsarafara@bocasay.com>
2020-12-03 09:28:33 +0300

    Generate filter and project gallery dynamically

--- src/components/project-section/project-section.vue
+++ src/components/project-section/project-section.vue
@@ -8,6 +8,84 @@
         name: "ProjectSection",
         components: {
             ProjectItem
+        },
+        data() {
+            return ({
+                projects: [
+                    {
+                        name: 'Lorem',
+                        image: 'http://via.placeholder.com/350x350',
+                        alt: 'Lorem ipsum',
+                        category: 'design'
+                    },
+                    {
+                        name: 'Lorem',
+                        image: 'http://via.placeholder.com/350x350',
+                        alt: 'Lorem ipsum',
+                        category: 'development'
+                    },
+                    {
+                        name: 'Lorem',
+                        image: 'http://via.placeholder.com/350x350',
+                        alt: 'Lorem ipsum',
+                        category: 'development'
+                    },
+                    {
+                        name: 'Lorem',
+                        image: 'http://via.placeholder.com/350x350',
+                        alt: 'Lorem ipsum',
+                        category: 'design'
+                    },
+                    {
+                        name: 'Lorem',
+                        image: 'http://via.placeholder.com/350x350',
+                        alt: 'Lorem ipsum',
+                        category: 'design'
+                    },
+                    {
+                        name: 'Lorem',
+                        image: 'http://via.placeholder.com/350x350',
+                        alt: 'Lorem ipsum',
+                        category: 'design'
+                    },
+                    {
+                        name: 'Lorem',
+                        image: 'http://via.placeholder.com/350x350',
+                        alt: 'Lorem ipsum',
+                        category: 'ipsum'
+                    },
+                    {
+                        name: 'Lorem',
+                        image: 'http://via.placeholder.com/350x350',
+                        alt: 'Lorem ipsum',
+                        category: 'lorem'
+                    },
+                    {
+                        name: 'Lorem',
+                        image: 'http://via.placeholder.com/350x350',
+                        alt: 'Lorem ipsum',
+                        category: 'lorem'
+                    }
+                ],
+                activeFilter: '*'
+            });
+        },
+        computed: {
+            categories() {
+                const categoryMap = new Map();
+                this.projects.forEach(item => categoryMap.set(item.category, 1));
+                return categoryMap.keys();
+            },
+            filteredProjects() {
+                return this.activeFilter === '*' ?
+                    this.projects :
+                    this.projects.filter(item => item.category === this.activeFilter);
+            }
+        },
+        methods: {
+            changeFilter(filter) {
+                this.activeFilter = filter;
+            }
         }
     }
 </script>

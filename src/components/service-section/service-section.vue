6c6bfad5c89137841ebb2173b9372886ce40580e
Randriantsarafara Ny Hasinavalona <hrandriantsarafara@bocasay.com>
2020-12-05 00:30:18 +0300

    Dynamize service list

--- src/components/service-section/service-section.vue
+++ src/components/service-section/service-section.vue
@@ -11,7 +11,39 @@
         },
         data() {
             return {
-                publicPath: process.env.BASE_URL
+                publicPath: process.env.BASE_URL,
+                services: [
+                    {
+                        description: 'Vestibulum condimentum vehicula massa, ac convallis nullarell',
+                        heading: 'Web Design',
+                        imageUrl: 'assets/images/services/port_services01.png'
+                    },
+                    {
+                        description: 'Vestibulum condimentum vehicula massa, ac convallis nullarell',
+                        heading: 'Photoshop',
+                        imageUrl: 'assets/images/services/port_services02.png'
+                    },
+                    {
+                        description: 'Vestibulum condimentum vehicula massa, ac convallis nullarell',
+                        heading: 'Marketing',
+                        imageUrl: 'assets/images/services/port_services03.png'
+                    },
+                    {
+                        description: 'Vestibulum condimentum vehicula massa, ac convallis nullarell',
+                        heading: 'Illustrator',
+                        imageUrl: 'assets/images/services/port_services04.png'
+                    },
+                    {
+                        description: 'Vestibulum condimentum vehicula massa, ac convallis nullarell',
+                        heading: 'Social Media',
+                        imageUrl: 'assets/images/services/port_services05.png'
+                    },
+                    {
+                        description: 'Vestibulum condimentum vehicula massa, ac convallis nullarell',
+                        heading: 'Graphics',
+                        imageUrl: 'assets/images/services/port_services06.png'
+                    }
+                ]
             };
         }
     };

45651a874ee9528aad9a6e7b2f76c54bef0fea6e
Randriantsarafara Ny Hasinavalona <hrandriantsarafara@bocasay.com>
2020-12-05 00:43:34 +0300

    Dynamize slide list

---
+++ src/components/testimonial-section/components/testimonial-box/testimonial-box.vue
@@ -0,0 +1,13 @@
+<template src="./testimonial-box.html">
+</template>
+
+<script>
+    export default {
+        name: "testimonial-box",
+        props: ['placeholder', 'video', 'content', 'author', 'position']
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/testimonial-section/testimonial-section.vue
+++ src/components/testimonial-section/testimonial-section.vue
@@ -1,10 +1,40 @@
 <template src="./testimonial-section.html">
-    
+
 </template>
 
 <script>
+    import TestimonialBox from './components/testimonial-box/testimonial-box';
     export default {
-        name: "TestimonialSection"
+        name: "TestimonialSection",
+        components: {
+            TestimonialBox
+        },
+        data() {
+            return ({
+                testimonials: [
+                    {
+                        placeholder: 'http://via.placeholder.com/854x510',
+                        video: 'https://www.youtube.com/embed/fp0cEiwxzQE',
+                        content: `Consectetur adipisicing elit sed do eiusmod tempor incididunt ut
+                                  labore et dolore magna aliqua t enim ad minim veniam, quis
+                                  nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                  commodo consequat. Duis aute irure dolor in reprehenderit.`,
+                        author: 'Carolyn Bernadette',
+                        position: 'UI Designer'
+                    },
+                    {
+                        placeholder: 'http://via.placeholder.com/854x510',
+                        video: 'https://www.youtube.com/embed/fp0cEiwxzQE',
+                        content: `Consectetur adipisicing elit sed do eiusmod tempor incididunt ut
+                                  labore et dolore magna aliqua t enim ad minim veniam, quis
+                                  nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                  commodo consequat. Duis aute irure dolor in reprehenderit.`,
+                        author: 'Carolyn Bernadette',
+                        position: 'UI Designer'
+                    }
+                ]
+            });
+        }
     }
 </script>
 

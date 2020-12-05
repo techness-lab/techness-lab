988a930cfb2e01ead5a9e6a9cc2dd212539d9f55
Randriantsarafara Ny Hasinavalona <hrandriantsarafara@bocasay.com>
2020-12-05 21:59:26 +0300

    Implement anchor tag routing

--- src/main.js
+++ src/main.js
@@ -3,12 +3,31 @@ import App from './App.vue'
 
 import VueTypedJs from 'vue-typed-js';
 import VueEllipseProgress from 'vue-ellipse-progress';
+import VueRouter from "vue-router";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
 Vue.use(VueTypedJs);
 Vue.use(VueEllipseProgress);
 
+const router = new VueRouter({
+    mode: 'history',
+    scrollBehavior: function(to) {
+        if (to.hash) {
+            return ({
+                selector: to.hash
+            });
+        } else {
+            return ({
+                x: 0,
+                y: 0
+            });
+        }
+    }
+});
+
 new Vue({
     render: h => h(App),
+    router
 }).$mount('#app');

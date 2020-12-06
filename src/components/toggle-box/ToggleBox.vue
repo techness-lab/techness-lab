44208e0c9b2d8d45bed78b27f97e2caad8356052
Randriantsarafara Ny Hasinavalona <hrandriantsarafara@bocasay.com>
2020-12-06 22:01:26 +0300

    Add menu toggling's business logic

--- src/components/toggle-box/ToggleBox.vue
+++ src/components/toggle-box/ToggleBox.vue
@@ -4,7 +4,17 @@
 
 <script>
     export default {
-        name: "ToggleBox"
+        name: "ToggleBox",
+        methods: {
+            togglePortBox() {
+                const body = document.querySelector('body');
+                if (!body.classList.contains('port_menu_open')) {
+                    body.classList.add('port_menu_open');
+                } else {
+                    body.classList.remove('port_menu_open');
+                }
+            }
+        }
     }
 </script>
 
